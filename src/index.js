a0b9f62e5f7587056531d0f07f4abfbfadcebaf3
Jeton Thaçi <jetonthaci@outlook.com>
2019-06-27 18:52:04 +0200

    add todo and project modules

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import pageLoad from './pageLoad';
+import Project from './project';
+import Todo from './todo';
 
 pageLoad();
+
+const todo1 = new Todo('Make lunch', 'Cook pasta', new Date(), 1);
+const project1 = new Project('My todos', todo1);
---
+++ src/project.js
@@ -0,0 +1,11 @@
+class Project {
+  constructor(name, todo) {
+    this.name = name;
+  }
+
+  // addTodo(todo) {
+  //   this.todos.push(todo);
+  // }
+}
+
+export { Project as default };
