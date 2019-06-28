217e2b28e5263a1ebfbd90a4fe3facd5f19227c0
Jeton Thaçi <jetonthaci@outlook.com>
2019-06-28 12:58:27 +0200

    add new projects and get projects

--- src/index.js
+++ src/index.js
@@ -2,9 +2,48 @@
 import Project from './project';
 import Todo from './todo';
 
-function getProject() {
-    
+function getProjects() {
+  const projects = {...localStorage};
+
+  if (Object.keys(projects).length === 0 && projects.constructor === Object) {
+    newProject('My todos');    
+  }
+
+  return projects;
 }
+
+function newProject(name) {
+  const p = new Project(name);
+  localStorage.setItem(p.name, JSON.stringify(p));
+}
+
+const newProjectField = document.querySelector('.new-project');
+const newProjectButton = document.querySelector('.add-project');
+newProjectButton.addEventListener('click', () => {
+  newProject(newProjectField.value);
+});
+
+// const todo1 = new Todo('Make lunch', 'Cook pasta', new Date(), 1);
+// const project1 = new Project('My todos', todo1);
+
+
+// const todo2 = new Todo('Make dinner', 'Salad', new Date(), 1);
+// const project2 = new Project('Other todo', todo2);
+
+// localStorage.setItem(project1.name, JSON.stringify(todo1));
+// localStorage.setItem(project2.name, JSON.stringify(todo2));
+
+
+// const p = {...localStorage};
+
+// for (var key in p) {
+//   if (p.hasOwnProperty(key)) {
+//       console.log(key + " -> " + p[key]);
+//   }
+// }
+
+
+// console.log(a)
 
 // const defaultProject = new Project('My todos');
 
@@ -24,9 +63,7 @@ function getProject() {
 //   return project;
 // }
 
-// function newProject(name) {
-//   return new Project(name);
-// }
+
 
 // get element with id addTodo
 // add event listener to it
