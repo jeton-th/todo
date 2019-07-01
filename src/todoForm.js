77b43c13e5c59b3d8dc991ccbea06376ab357c17
Jeton Thaçi <jetonthaci@outlook.com>
2019-07-01 15:35:25 +0200

    debug todoForm

--- src/todoForm.js
+++ src/todoForm.js
@@ -1,46 +1,52 @@
 import { Todo, addTodo } from './todo';
 
-const todoForm = (selectedProject) => {
-//   const content = document.querySelector('#content');
-//   const form = document.createElement('div');
-//   form.classList.add('todo-form');
+const todoform = (selectedProject) => {
+  const content = document.querySelector('#content');
+  const form = document.createElement('div');
+  form.classList.add('todo-form');
 
-//   const newTodoButton = document.createElement('input');
-//   newTodoButton.setAttribute('type', 'button');
-//   newTodoButton.classList.add('.add-todo');
+  const newTodoButton = document.createElement('input');
+  newTodoButton.setAttribute('type', 'button');
+  newTodoButton.setAttribute('value', 'Add todo');
+  newTodoButton.classList.add('.add-todo');
+  
+  const title = document.createElement('input');
+  title.setAttribute('placeholder', 'Title');
+  title.setAttribute('type', 'text');
+  title.classList.add('.title');
 
-//   const title = document.createElement('input');
-//   title.setAttribute('type', 'text');
-//   title.classList.add('.title');
+  const description = document.createElement('textarea');
+  description.setAttribute('placeholder', 'Description');
+  description.classList.add('.description');
 
-//   const description = document.createElement('textarea');
-//   description.setAttribute('placeholder', 'Description');
-//   description.classList.add('.description');
+  const dueDate = document.createElement('input');
+  dueDate.setAttribute('type', 'date');
+  dueDate.classList.add('.due-date');
 
-//   const dueDate = document.createElement('input');
-//   dueDate.setAttribute('type', 'date');
-//   dueDate.classList.add('.due-date');
+  const priority = document.createElement('input');
+  priority.setAttribute('type', 'number');
+  priority.classList.add('.priority');
 
-//   const priority = document.createElement('input');
-//   priority.setAttribute('type', 'number');
-//   priority.classList.add('.priority');
+  form.appendChild(title);
+  form.appendChild(description);
+  form.appendChild(dueDate);
+  form.appendChild(priority);
+  form.appendChild(newTodoButton);
+  content.appendChild(form);
 
-//   from.appendChild(newTodoButton);
-//   form.appendChild(title);
-//   form.appendChild(description);
-//   form.appendChild(dueDate);
-//   form.appendChild(priority);
-//   content.appendChild(form);
+  newTodoButton.addEventListener('click', () => {
+    const todo = new Todo(
+      title.value,
+      description.value,
+      dueDate.value,
+      priority.value
+    );
 
-  //   newTodoButton.addEventListener('click', () => {
-  //     const todo = new Todo(
-  //       title.value,
-  //       description.value,
-  //       dueDate.value,
-  //       priority.value
-  //     );
-  //     addTodo(todo, selectedProject);
-  //   });
+
+    addTodo(todo, selectedProject);
+  });
+
+
 };
 
-export { todoForm };
+export { todoform as default };
