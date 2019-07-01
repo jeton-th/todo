c3bbab37489e20f66e2f70f22182956bd3001e8b
Jeton Thaçi <jetonthaci@outlook.com>
2019-07-01 18:46:26 +0200

    make accordion work

--- src/render.js
+++ src/render.js
@@ -14,16 +14,6 @@ function showTodos() {
   todos.classList.add('todos');
   content.appendChild(todos);
 
-  // <div class="accordion" id="accordionExample">
-  //       <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-  //         Collapsible Group Item #1
-  //       </button>
-
-  //   <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
-  //       Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw de
-  //   </div>
-  // </div>
-
   for (let projectName in projects) {
     let project = document.createElement('h3');
     project.classList.add('project-name');
@@ -31,32 +21,46 @@ function showTodos() {
     todos.appendChild(project);
 
     let list = JSON.parse(projects[projectName]).store;
-    const accordion = document.createElement('div');
-    accordion.classList.add('accordion', 'accordionExample');
-    todos.appendChild(accordion);
+
 
     let i = 0;
     for (let key in list) {
+      const accordion = document.createElement('div');
+      accordion.id = 'accordionExample';
+      accordion.classList.add('accordion');
+      todos.appendChild(accordion);
+
       const button = document.createElement('button');
-      button.id = 'accordionExample';
       button.classList.add('btn', 'btn-link');
       button.setAttribute('type', 'button');
-      button.setAttribute('data-toggle', `#collapse${i}`);
-      button.setAttribute('data-target', 'collapse');
+      button.setAttribute('data-toggle', 'collapse');
+      button.setAttribute('data-target', `#collapse${i}`);
       button.setAttribute('aria-expanded', 'true');
       button.setAttribute('aria-controls', `collapse${i}`);
       button.innerHTML = list[key].title;
 
       const detail = document.createElement('div');
       detail.id = `collapse${i}`;
-      detail.classList.add('collapse','show');
+      detail.classList.add('collapse');
       detail.setAttribute('aria-labelledby', `heading${i}`);
-      button.setAttribute('data-parent', 'accordionExample');
-      detail.innerHTML = 'something'
+      button.setAttribute('data-parent', '#accordionExample');
+      
+      const description = document.createElement('p');
+      description.innerHTML = list[key].description;
+      detail.appendChild(description);
+
+      const date = document.createElement('p');
+      date.innerHTML = list[key].dueDate;
+      detail.appendChild(date);
+
+      const priority = document.createElement('p');
+      priority.innerHTML = list[key].priority;
+      detail.appendChild(priority);
 
       //add element for desc, priority and date
       accordion.appendChild(button);
       accordion.appendChild(detail);
+      i += 1;
     }
   }
 }
