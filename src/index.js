29dbf181cd181cd2ae256402e5b4c08329466343
Jeton Thaçi <jetonthaci@outlook.com>
2019-07-01 18:14:36 +0200

    add collapse to list

--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ import projectForm from './projectForm';
 import Project from './project';
 import Todo from './todo';
 import todoForm from './todoForm';
-import { showTodos,populateTodos } from './render';
+import { showTodos } from './render';
 
 projectForm();
 todoForm();
 showTodos();
-populateTodos()
--- src/render.js
+++ src/render.js
@@ -14,22 +14,40 @@ function showTodos() {
   todos.classList.add('todos');
   content.appendChild(todos);
 
+  // <div class="accordion" id="accordionExample">
+  //       <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+  //         Collapsible Group Item #1
+  //       </button>
+
+  //   <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+  //       Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw de
+  //   </div>
+  // </div>
+
   for (let projectName in projects) {
-    let project = document.createElement('p');
+    let project = document.createElement('h3');
     project.classList.add('project-name');
     project.innerHTML = projectName;
     todos.appendChild(project);
 
     let list = JSON.parse(projects[projectName]).store;
-    const ul = document.createElement('ul');
-    todos.appendChild(ul);
+    const accordion = document.createElement('div');
+    accordion.classList.add('accordion', 'accordionExample');
+    todos.appendChild(accordion);
 
+    let i = 0;
     for (let key in list) {
-      const li = document.createElement('li');
-      li.innerHTML = list[key].title;
+      const button = document.createElement('button');
+      button.classList.add('btn', 'btn-link');
+      button.setAttribute('type', 'button');
+      button.setAttribute('data-toggle', `#collapseOne${i}`);
+      button.setAttribute('data-target', 'collapse');
+      button.setAttribute('aria-expanded', 'true');
+      button.setAttribute('aria-controls', `collapseOne${i}`);
+      button.innerHTML = list[key].title;
 
       //add element for desc, priority and date
-      ul.appendChild(li);
+      accordion.appendChild(button);
     }
   }
 }
